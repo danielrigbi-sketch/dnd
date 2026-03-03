bc8d31c50eafbb0ce9ed573594c9baa9ad013fb7
danielrigbi-sketch <daniel.rigbi@gmail.com>
2026-03-03 14:25:57 +0200

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,12 +1,15 @@
+// הגדרות Firebase
 export const firebaseConfig = {
     apiKey: "AIzaSyBmKG3lCfoHK4bTdjUFSMR2YLmgbXtTmbM",
     authDomain: "dnd-dice-room.firebaseapp.com",
     databaseURL: "https://dnd-dice-room-default-rtdb.firebaseio.com",
     projectId: "dnd-dice-room",
     storageBucket: "dnd-dice-room.firebasestorage.app",
-    messagingSenderId: "365914207851", appId: "1:365914207851:web:777478485eabf5bf1f632e"
+    messagingSenderId: "365914207851", 
+    appId: "1:365914207851:web:777478485eabf5bf1f632e"
 };
 
+// צורות הקוביות ב-SVG
 export const diceShapes = {
     d4: '<polygon points="50,5 95,85 5,85" stroke="black" fill-opacity="0.95"/>',
     d6: '<rect x="15" y="15" width="70" height="70" rx="8" stroke="black" fill-opacity="0.95"/>',
