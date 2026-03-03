bb8ec0d82f9b1af7a678af821731287bb184a521
danielrigbi-sketch <daniel.rigbi@gmail.com>
2026-03-03 14:48:39 +0200

    Create audio.js

---
+++ audio.js
@@ -0,0 +1,36 @@
+const rollSound = new Audio('./dice.mp3');
+const critSound = new Audio('./crit.mp3');
+const failSound = new Audio('./fail.mp3');
+
+// פונקציית עזר לעצירת כל הסאונדים
+export function stopAllSounds() {
+    [rollSound, critSound, failSound].forEach(s => {
+        s.pause();
+        s.currentTime = 0;
+    });
+}
+
+// "שחרור" האודיו בנייד בצורה שקטה
+export function unlockAudio() {
+    [rollSound, critSound, failSound].forEach(s => {
+        s.muted = true;
+        s.play().then(() => {
+            s.pause();
+            s.currentTime = 0;
+            s.muted = false;
+        }).catch(() => {});
+    });
+}
+
+// ניגון סאונד בהתאם לתוצאה
+export function playRollSound(type, res, isMuted) {
+    if (isMuted) return;
+    stopAllSounds();
+    if (type === 'd20' && res === 20) {
+        critSound.play().catch(() => {});
+    } else if (type === 'd20' && res === 1) {
+        failSound.play().catch(() => {});
+    } else {
+        rollSound.play().catch(() => {});
+    }
+}
