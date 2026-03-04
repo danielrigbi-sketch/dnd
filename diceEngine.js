1970098dad5debb276ac7ee8a0f465d0db509c03
danielrigbi-sketch <daniel.rigbi@gmail.com>
2026-03-04 13:24:45 +0200

    Update diceEngine.js

--- diceEngine.js
+++ diceEngine.js
@@ -7,15 +7,15 @@ let diceBox;
 // פונקציית האתחול - נקרא לה פעם אחת כשהאפליקציה עולה
 export async function initDiceEngine() {
     diceBox = new DiceBox({
-        container: "#dice-box-canvas",
+        container: "#dice-box-canvas", // מצביע לקונטיינר שיהיה בתוך הזירה
         origin: "https://unpkg.com/@3d-dice/dice-box@1.1.3/dist/",
-        assetPath: "https://unpkg.com/@3d-dice/dice-box@1.1.3/dist/assets/", // הכתובת המלאה לרשת
+        assetPath: "assets/",
         theme: "default",
-        scale: 10, 
-        gravity: 4,
+        scale: 8, // הגדלה משמעותית כדי שיראו מצוין בנייד ובלפטופ
+        gravity: 4, // כוח משיכה חזק יותר לנחיתה יציבה
         friction: 0.8,
-        sounds: true, // המנוע ינגן את צלילי הפגיעה מהרשת
-        settleTimeout: 3000
+        sounds: true,
+        settleTimeout: 3000 // זמן המתנה לעצירת הקובייה
     });
 
     await diceBox.init();
