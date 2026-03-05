6cb3c06bc4c9304625a14c3740fbc00d38b78428
danielrigbi-sketch <daniel.rigbi@gmail.com>
2026-03-05 20:19:45 +0200

    Update lobby.js

--- js/lobby.js
+++ js/lobby.js
@@ -1,7 +1,7 @@
 // lobby.js - Welcome screen and Authentication Controller
 
-import * as db from "./firebaseService.js?v=100";
-import { startGame } from "./app.js?v=100";
+import * as db from "./firebaseService.js?v=101";
+import { startGame } from "./app.js?v=101";
 
 const authScreen = document.getElementById('auth-screen');
 const lobbyScreen = document.getElementById('lobby-screen');
