288fcccda28f7b8f3618107cd0082f7aa02bc91e
danielrigbi-sketch <daniel.rigbi@gmail.com>
2026-03-06 15:48:38 +0200

    Sprint 5: js/lobby.js v121

--- js/lobby.js
+++ js/lobby.js
@@ -1,7 +1,7 @@
 // lobby.js - Welcome screen and Authentication Controller
-import * as db from "./firebaseService.js?v=120";
-import { startGame } from "./app.js?v=120";
-import { setLanguage, getLang, t, updateDOM } from "./i18n.js?v=120";
+import * as db from "./firebaseService.js?v=121";
+import { startGame } from "./app.js?v=121";
+import { setLanguage, getLang, t, updateDOM } from "./i18n.js?v=121";
 
 const langToggleBtn = document.getElementById('lang-toggle-btn');
 langToggleBtn.innerText = getLang() === 'he' ? 'English' : 'עברית';
